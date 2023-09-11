0af2c0362beb902dded35fb0e0c1eaab8233cd06
ddelemosmarques <126253351+ddelemosmarques@users.noreply.github.com>
2023-09-11 10:06:35 -0300

    Add files via upload

---
+++ ex 01.go
@@ -0,0 +1,25 @@
+//Faça um algoritmo que leia
+//dois números inteiros e
+//mostre o maior deles.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var n1 int
+	var n2 int
+
+	fmt.Print(" Escreva um numero\n")
+	fmt.Scan(&n1)
+
+	fmt.Print(" Escreva o segundo\n")
+	fmt.Scan(&n2)
+
+	if n1 > n2 {
+		fmt.Printf("%d é maior que %d", n1, n2)
+	} else {
+		fmt.Printf("%d é maior que %d", n2, n1)
+	}
+}
