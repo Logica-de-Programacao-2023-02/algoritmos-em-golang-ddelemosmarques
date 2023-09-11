2df33482416aaae0664aee2fd8f4007c99d0c4b4
ddelemosmarques <126253351+ddelemosmarques@users.noreply.github.com>
2023-09-11 15:52:24 -0300

    Add files via upload

--- sequenciais 04.go
+++ sequenciais 04.go
@@ -1,21 +1,40 @@
-//Faça um algoritmo que leia três números reais
-//e mostre a média ponderada entre eles, com pesos 2, 3 e 5, respectivamente.
+//Faça um algoritmo que leia a
+// altura e o sexo de uma pessoa e mostre
+//se ela está abaixo, dentro ou acima do peso ideal,
+//utilizando as fórmulas do exercício 3 da lista de algoritmos sequenciais.
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	var n1, n2, n3 int
+	var p, a, imc float64
+	var s string
 
-	var p1 = 2
-	var p2 = 3
-	var p3 = 5
+	fmt.Printf("Qual é o seu peso? ")
+	fmt.Scan(&p)
+	fmt.Printf("Qual é a sua altura?")
+	fmt.Scan(&a)
+	fmt.Printf("Qual é seu sexo? ")
+	fmt.Scan(&s)
 
-	fmt.Println("Escreava 3 números")
-	fmt.Scan(&n1, &n2, &n3)
-	fmt.Println("A média ponderada destes numeros é ")
-	fmt.Println((n1*p1 + n2*p2 + n3*p3) / (p1 + p2 + p3))
+	imc = p / (a * a)
+
+	if imc < 18.5 {
+		fmt.Printf("O cálculo IMC indica que vc esta com Baixo peso")
+	} else if imc > 18.5 && imc < 24.9 {
+		fmt.Printf("O cálculo IMC indica que vc esta com Peso normal")
+	} else if imc > 25 && imc < 29.9 {
+		fmt.Printf("O cálculo IMC indica que vc testa com Sobrepeso")
+	} else if imc > 30 && imc < 34.9 {
+		fmt.Printf("O cálculo IMC indica que vc testa com Obesidade grau I")
+	} else if imc > 35 && imc < 39.9 {
+		fmt.Printf("O cálculo IMC indica que vc testa com Obesidade grau II")
+	} else {
+		fmt.Printf("O cálculo IMC indica que vc testa com Obesidade grau III")
+	}
 
 }
